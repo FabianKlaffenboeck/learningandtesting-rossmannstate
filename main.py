63c41f52a843353bd206d1072d43f09e0801f9b0
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2025-01-14 22:04:07 +0100

    added possible states

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from request_data import get_film_state
 if __name__ == '__main__':
     data = get_film_state(888621,3678)
     print(data)
+
+ # states:
+ # {'status': 'success', 'message': '', 'data': {'bagTrackingData': {}}}
+ # {'status': 'success', 'message': '', 'data': {'bagTrackingData': {'bagOrder': {'bagid': '888621', 'city': 'Passau', 'inDate': '2025-01-14T11:02:00+01:00', 'outletid': '203678', 'storeDescription': '', 'storeName': 'Dirk Rossmann GmbH', 'street': 'Kapuzinerstr. 30', 'zip': '94032'}}}}
