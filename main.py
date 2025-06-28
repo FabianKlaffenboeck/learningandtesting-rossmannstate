3fc81830ee0cb363b5a3c6236ea3c12f39a7028e
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2025-06-28 09:14:38 +0200

    run for list

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from typing import List
+
 from request_data import get_film_state
 
 if __name__ == '__main__':
-    data = get_film_state(888621,3678)
-    print(data)
+    list_of_films: List[int] = [208639, 208642, 308433, 308641]
 
- # states:
- # {'status': 'success', 'message': '', 'data': {'bagTrackingData': {}}}
- # {'status': 'success', 'message': '', 'data': {'bagTrackingData': {'bagOrder': {'bagid': '888621', 'city': 'Passau', 'inDate': '2025-01-14T11:02:00+01:00', 'outletid': '203678', 'storeDescription': '', 'storeName': 'Dirk Rossmann GmbH', 'street': 'Kapuzinerstr. 30', 'zip': '94032'}}}}
+    for film in list_of_films:
+        print(get_film_state(film, 3678))
+
+# states:
+# {'status': 'success', 'message': '', 'data': {'bagTrackingData': {}}}
+# {'status': 'success', 'message': '', 'data': {'bagTrackingData': {'bagOrder': {'bagid': '888621', 'city': 'Passau', 'inDate': '2025-01-14T11:02:00+01:00', 'outletid': '203678', 'storeDescription': '', 'storeName': 'Dirk Rossmann GmbH', 'street': 'Kapuzinerstr. 30', 'zip': '94032'}}}}
